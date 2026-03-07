288acba87b89ca559ac7ab0eb1cf3b3e149524cb
Rubén Laguna <ruben.laguna@gmail.com>
2026-03-07 15:34:13 +0100

    Refactor code so that logger setup is done first

--- src/pydantic_cli_minimal_example/cli.py
+++ src/pydantic_cli_minimal_example/cli.py
@@ -11,25 +11,26 @@ logger = logging.getLogger("myapp")
 
 def canoodle(settings: CanoodleSettings) -> None:
     logger.info("canoodle")
+    r = requests.get("https://google.com")
 
 
 def drizzle(settings: DrizzleSettings) -> None:
     logger.info("drizzle")
+    r = requests.get("https://google.com")
 
 
 def main() -> None:
     settings = get_settings()
 
+    requests_log_level = settings.requests_log_level
+    requests_numeric_level = getattr(logging, requests_log_level.upper(), logging.INFO)
+    logging.getLogger("urllib3").setLevel(requests_numeric_level)
+    setup_handlers()
+    logger.info("settings: %s", settings.model_dump_json())
+
     subcommand = get_subcommand(settings)
+
     if isinstance(subcommand, CanoodleSettings):
         canoodle(subcommand)
     elif isinstance(subcommand, DrizzleSettings):
         drizzle(subcommand)
-
-    setup_handlers()
-
-    requests_log_level = settings.requests_log_level
-    requests_numeric_level = getattr(logging, requests_log_level.upper(), logging.INFO)
-    logging.getLogger("urllib3").setLevel(requests_numeric_level)
-    logger.info("settings: %s", settings.model_dump_json())
-    r = requests.get("https://google.com")
