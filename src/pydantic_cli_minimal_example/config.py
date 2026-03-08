28d4908e155905f5d17ac30977cea2e8581b782e
Rubén Laguna <ruben.laguna@gmail.com>
2026-03-08 14:28:44 +0100

    Use pyjwt to validate jwt config parameter

--- src/pydantic_cli_minimal_example/config.py
+++ src/pydantic_cli_minimal_example/config.py
@@ -1,8 +1,50 @@
 import os
 from enum import Enum
 from pathlib import Path
+from typing import Annotated, override
 
+import jwt
+from pydantic import AfterValidator, model_validator
+from pydantic.types import Secret
 from pydantic_settings import BaseSettings, CliSubCommand, SettingsConfigDict
+from typing_extensions import Self
+
+
+class JsonWebTokenBase(
+    Secret[str]
+):  # This is not a Model so you can't use model_validator
+
+    @override
+    def _display(self) -> str:
+        # return "".join(["*" for x in self.get_secret_value()])
+
+        sub = jwt.decode(
+            self.get_secret_value(),
+            options={
+                "verify_signature": False,
+            },
+        ).get("sub")
+        return f"JWT sub: {sub}"
+
+
+def validate_jwt(v: str) -> str:
+    try:
+        jwt.decode(
+            v.get_secret_value(),
+            options={
+                "verify_signature": False,
+                "verify_iat": True,
+                # "verify_exp": True,
+                "verify_nbf": True,
+            },
+        )
+
+        return v
+    except jwt.exceptions.InvalidTokenError as e:
+        raise ValueError(e)
+
+
+JsonWebToken = Annotated[JsonWebTokenBase, AfterValidator(validate_jwt)]
 
 
 # This enum provides validation
@@ -31,6 +73,7 @@ class AppSettings(BaseSettings):
     requests_log_level: LogLevel = LogLevel.WARNING
     log_dir: Path = Path("./logs")
     log_file: str = "log"
+    jwt: JsonWebToken
 
     model_config = SettingsConfigDict(
         env_file=".env",  # load settings / configuration parameters if it exists
